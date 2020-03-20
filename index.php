054c956103da8d716c97ca2eb47b6245c1e78751
Robert Qiu <robertqiu@berkeley.edu>
2020-03-20 16:29:01 -0700

    hopefully calendar php starts working now

--- index.php
+++ index.php
@@ -45,17 +45,111 @@
                     <a href="events.php"><img src="images/infosession_sp2020.png" style="width:100%; height: auto"></img></a>
                     </br>
                     </br>
-                    <a href="https://www.facebook.com/events/605914039936996/" class="no-hover"><p class="event event-main">9/10: First General Meeting @ Hearst Field Annex, Room B5 [8PM - 9PM]</p></a>
+                    <!--<a href="https://www.facebook.com/events/605914039936996/" class="no-hover"><p class="event event-main">9/10: First General Meeting @ Hearst Field Annex, Room B5 [8PM - 9PM]</p></a>
                     <p class="event event-sub">9/19: Second General Meeting @ Wheeler 108 [8PM - 9:30PM]</p>
                     <p class="event event-main">9/27 - 9/29: Bear Jams @ Cory 293</p>
                     <a href="https://www.facebook.com/events/1112199975652802/?active_tab=about" class="no-hover"><p class="event event-main">10/1: Activision Tech Talk & Company Info Session @ Evans 60 [6PM - 9PM]</p></a>
-                    <p class="event event-main">2/5: Infosession @ Barrows 60 [8PM - 10PM]</p>
+                    <p class="event event-main">2/5: Infosession @ Barrows 60 [8PM - 10PM]</p>-->
+                    <?php 
+                        require __DIR__ . '/vendor/autoload.php';
+
+                        /**
+                         * Returns an authorized API client.
+                         * @return Google_Client the authorized client object
+                         */
+                        function getClient()
+                        {
+                            $client = new Google_Client();
+                            $client->setApplicationName('Google Calendar API PHP Quickstart');
+                            $client->setScopes(Google_Service_Calendar::CALENDAR_READONLY);
+                            $client->setAuthConfig(__DIR__ .'credentials.json');
+                            $client->setAccessType('offline');
+                            $client->setPrompt('select_account consent');
+
+                            // Load previously authorized token from a file, if it exists.
+                            // The file token.json stores the user's access and refresh tokens, and is
+                            // created automatically when the authorization flow completes for the first
+                            // time.
+                            $tokenPath = 'token.json';
+                            if (file_exists($tokenPath)) {
+                                $accessToken = json_decode(file_get_contents($tokenPath), true);
+                                $client->setAccessToken($accessToken);
+                            }
+
+                            // If there is no previous token or it's expired.
+                            if ($client->isAccessTokenExpired()) {
+                                // Refresh the token if possible, else fetch a new one.
+                                if ($client->getRefreshToken()) {
+                                    $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+                                } else {
+                                    // Request authorization from the user.
+                                    $authUrl = $client->createAuthUrl();
+                                    printf("Open the following link in your browser:\n%s\n", $authUrl);
+                                    print 'Enter verification code: ';
+                                    $authCode = trim(fgets(STDIN));
+
+                                    // Exchange authorization code for an access token.
+                                    $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+                                    $client->setAccessToken($accessToken);
+
+                                    // Check to see if there was an error.
+                                    if (array_key_exists('error', $accessToken)) {
+                                        throw new Exception(join(', ', $accessToken));
+                                    }
+                                }
+                                // Save the token to a file.
+                                if (!file_exists(dirname($tokenPath))) {
+                                    mkdir(dirname($tokenPath), 0700, true);
+                                }
+                                file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+                            }
+                            return $client;
+                        }
+
+
+                        // Get the API client and construct the service object.
+                        $client = getClient();
+                        $service = new Google_Service_Calendar($client);
+
+                        // Print the next 10 events on the user's calendar.
+                        $calendarId = 'primary';
+                        $optParams = array(
+                          'maxResults' => 6,
+                          'orderBy' => 'startTime',
+                          'singleEvents' => true,
+                          'timeMin' => date('c', strtotime("-1 days")),
+                        );
+                        $results = $service->events->listEvents($calendarId, $optParams);
+                        $events = $results->getItems();
+
+                        if (empty($events)) {
+                            //print "No upcoming events found.\n";
+                        } else {
+                            //print "Upcoming events:\n";
+                            foreach ($events as $event) {
+                                $start = $event->start->dateTime;
+                                $timeStuff = '';
+                                if (empty($start)) {
+                                    $start = $event->start->date;
+                                } else {
+                                    if (!empty($event->end->dateTime)) {
+                                        $startTime = date('g:iA', strtotime($start));
+                                        $endTime = date('g:iA', strtotime($event->end->dateTime));
+                                        $timeStuff = ' [' + $startTime + ' - ' + $endTime + ']';
+                                    }
+                                }
+                                //printf("%s (%s)\n", $event->getSummary(), $start);
+                                $finalDate = date('m/d', strtotime($start));
+                                echo '<p class="event event-main">' + $finalDate + ': ' + $event->getSummary() + ' @ ' + $event->getLocation() + $timeStuff + '</p>';
+                            }
+                        }
+                    ?>
                     </br>
                 <h2 class="section-title">CONNECT TO OUR DISCORD!</h2>
                 <iframe src="https://discordapp.com/widget?id=433080296057864192&theme=dark" width="350" height="500"
                     allowtransparency="true" frameborder="0"></iframe>
                 <br><br>
-                <h2 class="section-title">BEAR JAMS FALL 2018: WINNER</h2>
+                <h2 class="section-title">BEAR JAMS FALL 2019: WINNER</h2>
                 <iframe style="padding-left:0%; width: 80%" src="https://itch.io/embed/489882" height="167" width="552"
                     frameborder="0"></iframe>
                 <br><br>
