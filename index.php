0ff1ebfe9266bc4fdf3295b287a1fa48583b570b
Robert Qiu <robertqiu@berkeley.edu>
2021-01-17 14:38:52 -0800

    some fixes

--- index.php
+++ index.php
@@ -76,7 +76,7 @@
             		<h2 class="section-title">Featured Events <a href="club.php?tab=events">Find Out More</a></h2>
             		<a href="https://www.facebook.com/Game-Design-Development-at-Berkeley-1191501494351583/events/"><img src="images/infosesssionsp2021.png" style="width:100%; height: auto; border-radius: 4px;"></img></a>
             		<br/><br/>
-            		<a href="https://www.facebook.com/Game-Design-Development-at-Berkeley-1191501494351583/events/"><img src="images/Decalinfosesssionsp2021.png" style="width:100%; height: auto; border-radius: 4px;"></img></a>
+            		<a href="https://www.facebook.com/events/1468921663313123"><img src="images/Decalinfosesssionsp2021.png" style="width:100%; height: auto; border-radius: 4px;"></img></a>
             	</div>
             </div>
             <div class="section-main">
